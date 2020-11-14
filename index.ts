a97f24970ff2d946738ba7c5cec458711fff20de
萧泳森 <603540681@qq.com>
2020-11-14 18:39:25 +0800

    feat: 根据不同 url 返回不同文件

---
+++ index.ts
@@ -0,0 +1,51 @@
+// 1. 根据 url 返回不同的文件
+// 2. 处理查询参数
+// 3. 匹配任意文件
+// 4. 处理不存在的文件
+// 5. 处理非 get 请求
+// 6. 添加缓存选项
+// 7. 响应内容启用 gzip
+import * as http from 'http';
+import * as fs from 'fs';
+import * as p from 'path';
+
+const server = http.createServer();
+const publicDir = p.resolve(__dirname, 'public')
+
+server.on('request', (request, response) => {
+  const { method, url, headers } = request
+  switch(url){
+    case '/index.html':
+      response.setHeader('Content-Type', 'text/html; charset=utf-8;')
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
+        if(error) {
+          throw error;
+        }
+        response.end(data.toString())
+      });
+      break; 
+    case '/main.js':
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8;')
+      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
+        if(error) {
+          throw error;
+        }
+        response.end(data.toString())
+      });
+      break; 
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css; charset=utf-8;')
+      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
+        if(error) {
+          throw error;
+        }
+        response.end(data.toString())
+      });
+      break; 
+  }
+
+})
+
+server.listen(8080,() => {
+  console.log('服务启动了.')
+})
